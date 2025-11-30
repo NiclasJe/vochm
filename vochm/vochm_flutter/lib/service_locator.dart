66a4b505fd96bf10eea93783bc684446bd19d2ed
NiclasJe <78955217+NiclasJe@users.noreply.github.com>
2025-11-30 15:04:50 +0100

    Set db host to db vm ip

--- vochm/vochm_flutter/lib/service_locator.dart
+++ vochm/vochm_flutter/lib/service_locator.dart
@@ -10,7 +10,7 @@ final getIt = GetIt.instance;
 
 Future<void> setupServiceLocator({String? serverUrl}) async {
   // Serverpod Client
-  final url = 'http://172.24.4.239:5432/';
+  final url = 'http://172.24.4.239:8080/';
   final client = Client(url)..connectivityMonitor = FlutterConnectivityMonitor();
 
   getIt.registerLazySingleton<Client>(() => client);
